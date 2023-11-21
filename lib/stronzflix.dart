29ec0034692a931a0e48b5629c20220d4e37403f
Bonfra04 <bonfract@gmail.com>
2023-11-21 15:19:44 +0100

    now works with android tv

--- lib/stronzflix.dart
+++ lib/stronzflix.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stronzflix/home.dart';
 
 class Stronzflix extends StatelessWidget {
@@ -6,20 +7,25 @@ class Stronzflix extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Stronzflix',
-            theme: ThemeData(
-                colorScheme: ColorScheme.dark(
-                    brightness: Brightness.dark,
-                    primary: Colors.orange,
-                    background: (Colors.grey[900])!,
-                    surface: const Color(0xff121212),
-                    surfaceTint: Colors.transparent,
+        return Shortcuts(
+            shortcuts: <LogicalKeySet, Intent>{
+                LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
+            },
+            child: MaterialApp(
+                title: 'Stronzflix',
+                theme: ThemeData(
+                    colorScheme: ColorScheme.dark(
+                        brightness: Brightness.dark,
+                        primary: Colors.orange,
+                        background: (Colors.grey[900])!,
+                        surface: const Color(0xff121212),
+                        surfaceTint: Colors.transparent,
+                    ),
+                    useMaterial3: true,
                 ),
-                useMaterial3: true,
-            ),
-            home: const HomePage(),
-            debugShowCheckedModeBanner: false,
+                home: const HomePage(),
+                debugShowCheckedModeBanner: false
+            )
         );
     }
 }
