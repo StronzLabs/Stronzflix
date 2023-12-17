74012aa35e87605f54c2b692a96be51e32cdf981
Bonfra04 <bonfract@gmail.com>
2023-12-17 18:41:33 +0100

    support for web platform on the video player

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 final class SPlatform {
-    static bool get isDesktop => Platform.isWindows || Platform.isLinux;
-    static bool get isMobile => Platform.isAndroid || Platform.isIOS;
+    static bool get isWeb => kIsWeb;
+    static bool get isDesktop => !SPlatform.isWeb && (Platform.isWindows || Platform.isLinux);
+    static bool get isMobile => !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
 
     SPlatform._();
 }
