377a596f37648fa4be4dcf942eca0ebcf517f399
Bonfra04 <bonfract@gmail.com>
2023-12-18 12:46:04 +0100

    better code abstraction

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:url_launcher/url_launcher.dart';
 
 final class SPlatform {
     static bool get isWeb => kIsWeb;
@@ -7,4 +8,13 @@ final class SPlatform {
     static bool get isMobile => !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
 
     SPlatform._();
+
+    static Future<void> launchURL(String url) async {
+        Uri uri = Uri.parse(url);
+        if (await canLaunchUrl(uri)) {
+            await launchUrl(Uri.parse(url));
+        } else {
+            throw 'Could not launch $url';
+        }
+    }
 }
--- lib/utils/simple_http.dart
+++ lib/utils/simple_http.dart
@@ -1,3 +1,10 @@
 import 'package:http/http.dart' as http;
 
-Future<String> get(String url, {Map<String, String>? headers}) async => (await http.get(Uri.parse(url), headers: headers)).body;
+Future<String> get(String url, {Map<String, String>? headers}) async {
+
+    headers ??= {};
+    if(!headers.containsKey("User-Agent"))
+        headers["User-Agent"] = "Stronzflix";
+
+    return (await http.get(Uri.parse(url), headers: headers)).body;
+}
