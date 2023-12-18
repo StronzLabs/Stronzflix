d5e9ceb118f4cb4b3a6321aefb136b2f3cced56a
Bonfra04 <bonfract@gmail.com>
2023-12-18 15:18:06 +0100

    planning support for WebOS

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import "package:universal_html/html.dart" as html;
 import 'package:url_launcher/url_launcher.dart';
 
 final class SPlatform {
     static bool get isWeb => kIsWeb;
     static bool get isDesktop => !SPlatform.isWeb && (Platform.isWindows || Platform.isLinux);
     static bool get isMobile => !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
+
+    static bool get isWebOS => SPlatform.isWeb && html.window.navigator.userAgent.contains("WebOS");
 
     SPlatform._();
 
