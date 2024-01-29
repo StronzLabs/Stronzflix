dd7375fce0943bab07fccb558cc26bd7110f0095
Bonfra04 <bonfract@gmail.com>
2024-01-29 19:13:19 +0100

    and obviously i forgot about linux

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Backend.init();
 
-    registerWith(options: {'platforms': [ 'linux', 'windows' ], 'video.decoders': [ 'DXVA' ]});
+    registerWith(options: {'platforms': [ 'linux', 'windows' ], 'video.decoders': [ 'DXVA', 'FFmpeg' ]});
     if(SPlatform.isMobile)
         await FlutterDownloader.initialize();
     if(SPlatform.isDesktop)
