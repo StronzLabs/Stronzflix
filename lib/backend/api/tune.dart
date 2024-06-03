f689c0ab605fddf03b06aa11a58562cadb6b6682
Bonfra04 <bonfract@gmail.com>
2024-06-03 12:39:31 +0200

    updated tld

--- lib/backend/api/tune.dart
+++ lib/backend/api/tune.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:stronzflix/utils/simple_http.dart' as http;
 
 class Tuner {
@@ -11,12 +10,17 @@ class Tuner {
         return this.validator(body);
     }
 
+    Future<List<String>> _getDomains() async {
+        String response = await http.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt");
+        return response.split("\n").where((String line) => !line.startsWith("#")).toList();
+    }
+
     Stream<dynamic> findDomain(String subdomain) async* {
         String? result;
         double progress = 0.0;
         yield progress;
 
-        List<String> domains = (await rootBundle.loadString('assets/tld_list.txt')).split("\n");
+        List<String> domains = await this._getDomains();
         
         for (String domain in domains) {
             if (await this.validateDomain("${subdomain}.${domain}")) {
