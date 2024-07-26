8ea198431120d266eafd51d9bacc85488528e7e7
Bonfra04 <bonfract@gmail.com>
2024-07-26 11:45:47 +0200

    no idea, trying to move things around

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
 import 'package:stronzflix/stronzflix.dart';
 
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
-    MediaKit.ensureInitialized(); 
     runApp(const Stronzflix());
 } 
