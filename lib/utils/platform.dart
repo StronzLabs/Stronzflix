9457c45714bd5a134de78736f512e7f908f0cfcb
Bonfra04 <bonfract@gmail.com>
2024-08-10 00:06:23 +0200

    better mobile detection

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -6,7 +6,7 @@ import 'package:url_launcher/url_launcher.dart';
 final class SPlatform {
     static bool get isWeb => kIsWeb;
     static bool get isDesktop => !SPlatform.isWeb && (Platform.isWindows || Platform.isLinux);
-    static bool get isMobile => !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
+    static bool get isMobile => !SPlatform.isTV && !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
 
     static bool get isMobileWeb => SPlatform.isMobile || SPlatform.isWeb && (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android);
     static bool get isDesktopWeb => SPlatform.isDesktop || SPlatform.isWeb && (defaultTargetPlatform == TargetPlatform.windows || defaultTargetPlatform == TargetPlatform.linux);
