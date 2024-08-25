c77d173b45bf36f8238c71250eadb9f4ee09aeb6
Bonfra04 <bonfract@gmail.com>
2024-08-25 21:15:25 +0200

    better platform detection

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -6,11 +6,11 @@ import 'package:window_manager/window_manager.dart';
 
 final class SPlatform {
     static bool get isWeb => kIsWeb;
-    static bool get isDesktop => !SPlatform.isWeb && (Platform.isWindows || Platform.isLinux);
+    static bool get isDesktop => !SPlatform.isWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS);
     static bool get isMobile => !SPlatform.isTV && !SPlatform.isWeb && (Platform.isAndroid || Platform.isIOS);
 
     static bool get isMobileWeb => SPlatform.isMobile || SPlatform.isWeb && (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android);
-    static bool get isDesktopWeb => SPlatform.isDesktop || SPlatform.isWeb && (defaultTargetPlatform == TargetPlatform.windows || defaultTargetPlatform == TargetPlatform.linux);
+    static bool get isDesktopWeb => SPlatform.isDesktop || SPlatform.isWeb && (defaultTargetPlatform == TargetPlatform.windows || defaultTargetPlatform == TargetPlatform.linux || defaultTargetPlatform == TargetPlatform.macOS);
 
     static late final bool _isTV;
     static bool get isTV => _isTV;
