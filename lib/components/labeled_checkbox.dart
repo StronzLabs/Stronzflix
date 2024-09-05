dfe095fc555a53cd03be250980a544ee6b968e20
Bonfra04 <bonfract@gmail.com>
2024-09-05 17:46:27 +0200

    labeled checkbox component

---
+++ lib/components/labeled_checkbox.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LabeledCheckbox extends StatefulWidget {
+    
+    final Widget label;
+    final void Function(bool)? onChanged;
+    final bool initialValue;
+    
+    const LabeledCheckbox({
+        super.key,
+        required this.label,
+        this.onChanged,
+        required this.initialValue
+    });
+
+    @override
+    State<StatefulWidget> createState() => _LabeledCheckboxState();
+
+}
+
+class _LabeledCheckboxState extends State<LabeledCheckbox> {
+
+    late bool _checked = super.widget.initialValue;
+
+    @override
+    Widget build(BuildContext context) {
+        return Row(
+            children: [
+                Checkbox(
+                    value: this._checked,
+                    onChanged: (value) {
+                        super.setState(() => this._checked = value ?? false);
+                        super.widget.onChanged?.call(this._checked);
+                    },
+                ),
+                super.widget.label
+            ],
+        );
+    }
+}
