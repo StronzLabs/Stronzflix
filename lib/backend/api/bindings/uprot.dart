6757a325184407256f2bf7ed1457fcd6a4154b17
Bonfra04 <bonfract@gmail.com>
2024-09-15 18:27:20 +0200

    assuming its already the framed version

--- lib/backend/api/bindings/uprot.dart
+++ lib/backend/api/bindings/uprot.dart
@@ -1,7 +1,6 @@
 import 'package:html/dom.dart';
 import 'package:stronzflix/backend/api/middleware.dart';
 import 'package:html/parser.dart' as html;
-import 'package:sutils/utils/simple_http.dart';
 
 class UProt extends Middleware {
     static Middleware instance = UProt._();
@@ -9,11 +8,6 @@ class UProt extends Middleware {
 
     @override
     Future<Uri> pass(Uri uri, String body) async {
-        if(!uri.path.contains("mse")) {
-            String id = uri.pathSegments.last;
-            uri = Uri.parse("${uri.scheme}://${uri.host}/mse/${id}");
-            body = await HTTP.get(uri);
-        }
         Document document = html.parse(body);
         String url = document.querySelector(".button")!.parent!.attributes["href"]!;
         return Uri.parse(url);
