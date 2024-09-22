a9a2f27f68fe34b233a9015173000adfe1599626
Bonfra04 <bonfract@gmail.com>
2024-09-22 22:56:35 +0200

    using embed to bypass captcha

--- lib/backend/api/bindings/uprot.dart
+++ lib/backend/api/bindings/uprot.dart
@@ -1,6 +1,7 @@
 import 'package:html/dom.dart';
 import 'package:stronzflix/backend/api/middleware.dart';
 import 'package:html/parser.dart' as html;
+import 'package:sutils/utils/simple_http.dart';
 
 class UProt extends Middleware {
     static Middleware instance = UProt._();
@@ -8,6 +9,9 @@ class UProt extends Middleware {
 
     @override
     Future<Uri> pass(Uri uri, String body) async {
+        String id = uri.pathSegments[1];
+        uri = Uri.parse("${uri.scheme}://${uri.host}/mse/${id}");
+        String body = await HTTP.get(uri);
         Document document = html.parse(body);
         String url = document.querySelector(".button")!.parent!.attributes["href"]!;
         return Uri.parse(url);
