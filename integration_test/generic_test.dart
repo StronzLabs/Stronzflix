2a3a1bc7644ac462fc40b7a97228516bbdd3817b
Bonfra04 <bonfract@gmail.com>
2025-01-27 12:27:31 +0100

    realtime logs

--- integration_test/generic_test.dart
+++ integration_test/generic_test.dart
@@ -38,37 +38,48 @@ Future<void> selectSite(WidgetTester tester, String site) async {
 Future<void> search(WidgetTester tester, String query) async {
     expect(find.byType(HomePage), findsOneWidget);
 
+    print("Tapping search icon");
     await tester.tap(find.byIcon(Icons.search));
     await tester.pumpAndSettle();
 
+    print("Selecting textfileld");
     Finder searchFinder = find.byType(TextField);
     expect(searchFinder, findsOneWidget);
 
+    print("Entering text");
     await tester.enterText(searchFinder, "A");
     await tester.testTextInput.receiveAction(TextInputAction.done);
     await tester.pumpAndSettle();
 
+    print("Expecting grid");
     expect(find.byType(SliverGrid), findsOneWidget);
 }
 
 Future<void> playFirstResult(WidgetTester tester, String query) async {
     expect(find.byType(HomePage), findsOneWidget);
 
+    print("Searching for ${query}");
     await search(tester, query);
-            
+        
+    print("Searching play icon");
     Finder playFinder = find.byIcon(Icons.play_arrow).first;
     expect(playFinder, findsAny);
 
+    print("Hitting play icon");
     await tester.tap(playFinder);
+    print("Awaiting settling");
     await tester.pumpAndSettle(const Duration(milliseconds: 100), EnginePhase.sendSemanticsUpdate, const Duration(seconds: 30));
 
+    print("Expeting player page");
     expect(find.byType(PlayerPage), findsOneWidget);
     expect(find.byType(Dialog), findsNothing);
+    print("OK");
 }
 
 void testPlaybackFor(String site) {
     print("testPlaybackFor ${site}");
     testWidgets(site, (WidgetTester tester) async {
+        print("ADDIO");
         await tester.runAsync(() async {
             print("Testing site");
             await Future.delayed(Durations.extralong4);
@@ -77,8 +88,12 @@ void testPlaybackFor(String site) {
             await TestHelper.pumpApp();
             print("Pump done");
             await Future.delayed(Durations.extralong4);
+            print("Selecting site");
             await selectSite(tester, site);
+            print("Selected site");
+            print("Playing first result");
             await playFirstResult(tester, "A");
+            print("Done");
         });
     });
 }
