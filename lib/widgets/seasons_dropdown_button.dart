d97f8596fa21eb0d06817d4e413ad44d9803084e
Bonfra04 <bonfract@gmail.com>
2025-03-09 13:40:38 +0100

    abstraced season dropdown button and action button

---
+++ lib/widgets/seasons_dropdown_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:stronzflix/backend/api/media.dart';
+
+class SeasonsDropdownButton extends StatelessWidget {
+    
+    final Season selectedSeason;
+    final List<Season> seasons;
+    final Function(Season) onSeasonSelected;
+
+    const SeasonsDropdownButton({
+        super.key,
+        required this.selectedSeason,
+        required this.seasons,
+        required this.onSeasonSelected
+    });
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Theme.of(context).disabledColor, width: 2.0),
+                borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: DropdownButton<Season>(
+                focusColor: Colors.transparent,
+                borderRadius: BorderRadius.circular(20.0),
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                underline: const SizedBox.shrink(),
+                value: this.selectedSeason,
+                items: [
+                    for (Season season in this.seasons)
+                        DropdownMenuItem(
+                            value: season,
+                            child: Text(season.name ?? "Stagione ${season.seasonNo}")
+                        )
+                ],
+                onChanged: this.seasons.length == 1 ? null : (selected) => this.onSeasonSelected(selected!)
+            )
+        );
+    }
+}
