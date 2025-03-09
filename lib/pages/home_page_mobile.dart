09a5c114a29d69e406bd0060f8efb15893b934b0
Bonfra04 <bonfract@gmail.com>
2025-03-09 14:16:30 +0100

    moved icon to widgets folder

--- lib/pages/home_page_mobile.dart
+++ lib/pages/home_page_mobile.dart
@@ -7,7 +7,7 @@ import 'package:stronzflix/backend/storage/settings.dart';
 import 'package:stronzflix/components/card_grid.dart';
 import 'package:stronzflix/components/delete_title_button.dart';
 import 'package:stronzflix/components/download_card.dart';
-import 'package:stronzflix/components/downloads_icon.dart';
+import 'package:stronzflix/widgets/downloads_icon.dart';
 import 'package:stronzflix/widgets/save_title_button.dart';
 import 'package:stronzflix/components/title_card.dart';
 import 'package:stronzflix/pages/home_page.dart';
